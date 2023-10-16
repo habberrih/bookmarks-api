f7f8b69abc40f0d89fc7554c55c461ade4434fce
habberrih <abdul2013ly@gmail.com>
2023-10-16 15:30:39 +0200

    feat: add dto for auth module

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+export class AuthDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
