8deafb586946ef522df198536dfe2539c7c669a4
habberrih <abdul2013ly@gmail.com>
2023-10-16 15:31:36 +0200

    refactor: create new user

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
-
-@Injectable({})
+import { PrismaService } from 'src/prisma/service/prisma.service';
+import { AuthDto } from '../dto';
+import * as argon from 'argon2';
+@Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   async login() {
     return 'I am logged in';
   }
-  async signup() {
-    return 'I am signed up';
+  async signup(signUpCred: AuthDto): Promise<any> {
+    const hashedPassword = await argon.hash(signUpCred.password);
+    const newUser = await this.prisma.user.create({
+      data: {
+        email: signUpCred.email,
+        name: signUpCred.name,
+        username: signUpCred.username,
+        password: hashedPassword,
+      },
+    });
+
+    delete newUser.password;
+    return newUser;
   }
 }
