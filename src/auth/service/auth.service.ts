08f6b63199801ef06becd01405f05e7eeb6dcca3
habberrih <abdul2013ly@gmail.com>
2023-10-17 19:21:55 +0200

    refactor: generate the access token of user

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,25 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/service/prisma.service';
-import { AuthDto } from '../dto';
+import { SignupDto, LoginDto } from '../dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { error } from 'console';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
-  async login() {
-    return 'I am logged in';
-  }
-  async signup(signUpCred: AuthDto): Promise<any> {
-    const hashedPassword = await argon.hash(signUpCred.password);
-    const newUser = await this.prisma.user.create({
-      data: {
-        email: signUpCred.email,
-        name: signUpCred.name,
-        username: signUpCred.username,
-        password: hashedPassword,
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+
+  async login(loginDto: LoginDto): Promise<object> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        username: loginDto.username,
       },
     });
+    if (!user) throw new ForbiddenException('Credentials not found');
 
-    delete newUser.password;
-    return newUser;
+    const passwordMatched = await argon.verify(
+      user.password,
+      loginDto.password,
+    );
+    if (!passwordMatched) throw new ForbiddenException('Credentials not found');
+
+    const token = await this.generateJwtToken(user.id, user.email);
+
+    return {
+      username: user.username,
+      name: user.name,
+      access_token: token,
+    };
+  }
+  async signup(signUpCred: SignupDto): Promise<object> {
+    const hashedPassword = await argon.hash(signUpCred.password);
+    try {
+      const newUser = await this.prisma.user.create({
+        data: {
+          email: signUpCred.email,
+          name: signUpCred.name,
+          username: signUpCred.username,
+          password: hashedPassword,
+        },
+      });
+
+      const token = await this.generateJwtToken(newUser.id, newUser.email);
+      return {
+        username: newUser.username,
+        name: newUser.name,
+        access_token: token,
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials are already taken.');
+        }
+      }
+    }
+    throw error;
+  }
+
+  generateJwtToken(userId: string, email: string): Promise<string> {
+    const payload = {
+      sub: userId,
+      email: email,
+    };
+
+    return this.jwt.signAsync(payload, {
+      secret: this.config.get('JWT_SECRET_KEY'),
+      expiresIn: this.config.get('JWT_EXPIRESIN'),
+    });
   }
 }
