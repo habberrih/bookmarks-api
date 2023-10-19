35e071423d8c39505e507bfb0bd77256c59bcda1
habberrih <abdul2013ly@gmail.com>
2023-10-19 17:14:52 +0200

    refactor: add signup endpoint test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,69 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
 import { AppModule } from './../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from '../src/prisma/service/prisma.service';
+import * as pactum from 'pactum';
+import passport from 'passport';
+import { timeout } from 'rxjs';
 
-describe('AppController (e2e)', () => {
+describe('App e2e test', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const appModuleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = appModuleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
     await app.init();
+    await app.listen(6000);
+
+    prisma = app.get(PrismaService);
+
+    await prisma.cleanDatabase();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    app.close();
+  });
+
+  describe('Auth Module', () => {
+    describe('Signup', () => {
+      it('should be return 201 when user is signed up', async () => {
+        return pactum
+          .spec()
+          .post('http://localhost:6000/api/auth/signup')
+          .withBody({
+            username: 'test',
+            name: 'test',
+            email: 'test@test.com',
+            password: 'password',
+          })
+          .expectStatus(201);
+      });
+    });
+
+    describe('Login', () => {});
+  });
+
+  describe('User Module', () => {
+    describe('Get Me', () => {});
+
+    describe('Edit User', () => {});
+  });
+
+  describe('Bookmark Module', () => {
+    describe('Create Bookmark', () => {});
+
+    describe('Get Bookmarks', () => {});
+
+    describe('Get Bookmark by Id', () => {});
+
+    describe('Edit Bookmark', () => {});
+
+    describe('Delete Bookmark', () => {});
   });
 });
