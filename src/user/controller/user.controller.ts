f938bf0908473c79ad7dfed1366ec8c7b5632d2e
habberrih <abdul2013ly@gmail.com>
2023-10-19 17:15:36 +0200

    refactor: change imports path to '../../'

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { GetCurrentUser } from 'src/auth/decorator';
-import { JwtGuard } from 'src/auth/guard';
+import { GetCurrentUser } from '../../auth/decorator';
+import { JwtGuard } from '../../auth/guard';
 
 @UseGuards(JwtGuard)
 @Controller('users')
