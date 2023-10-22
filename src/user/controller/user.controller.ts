a127909877c9b4f2d020b668614bf954b40fea70
habberrih <abdul2013ly@gmail.com>
2023-10-22 12:50:55 +0200

    feat: add function to edit the user info

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetCurrentUser } from '../../auth/decorator';
 import { JwtGuard } from '../../auth/guard';
+import { EditUserDto } from '../dto';
+import { UserService } from '../service/user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @Get('me')
   getMe(@GetCurrentUser() user: User) {
     return user;
+  }
+
+  @Patch(':id')
+  editUser(
+    @GetCurrentUser('id') userId: string,
+    @Body() editUserDto: EditUserDto,
+  ) {
+    return this.userService.editUser(userId, editUserDto);
   }
 }
--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/service/prisma.service';
+import { EditUserDto } from '../dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async editUser(userId: string, editUserDto: EditUserDto) {
+    const user = await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: { ...editUserDto },
+      select: {
+        email: true,
+        name: true,
+        username: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    return user;
+  }
+}
